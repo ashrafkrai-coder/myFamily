232a9897e2511756cf976f2d59afcb17afed6289
SMK TAMAN MEDAN PERANTI FASA 2 | GURU 15 <bea8613.g15@m365a1.moe.edu.my>
2026-07-08 12:31:40 +0800

    update v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tabung-fd-v5';
+const CACHE = 'tabung-fd-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
